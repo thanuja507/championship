537aecb3e5b20adc3467a3fb221e5ea598e04db5
thanuja507 <thanuja.uppuluri@gmail.com>
2024-06-11 14:32:13 -0400

    Initial code changes

---
+++ Program.cs
@@ -0,0 +1,41 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using F1Championship.Data;
+namespace F1Championship
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddDbContext<F1ChampionshipContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("F1ChampionshipContext") ?? throw new InvalidOperationException("Connection string 'F1ChampionshipContext' not found.")));
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,29 @@
+# F1 Championship | Software Engineering MVC Project 
+
+This project provides comprehensive online platform dedicated to the Formula 1 championship.
+
+Formula One, commonly known as Formula 1 or F1, is the highest class of international racing for open-wheel single-seater formula racing cars sanctioned by the Fédération Internationale de l'Automobile (FIA). The FIA Formula One World Championship has been one of the world's premier forms of racing since its inaugural running in 1950. The word formula in the name refers to the set of rules all participants' cars must follow. A Formula One season consists of a series of races, known as Grands Prix. Grands Prix take place in multiple countries and continents on either purpose-built circuits or closed public roads.
+
+## Data Model
+
+<img src="wwwroot/img/data_models.PNG">
+
+## Home Page
+
+<img src="wwwroot/img/home.PNG">
+
+## Teams Page
+
+<img src="wwwroot/img/team.PNG">
+
+## Drivers Page
+
+<img src="wwwroot/img/drivers.PNG">
+
+## Team Leagues Page
+
+<img src="wwwroot/img/team_leagues.PNG">
+
+## Dashboards
+
+<img src="wwwroot/img/dashboards.PNG">
